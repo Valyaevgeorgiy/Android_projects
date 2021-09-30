c8270da06b4348aa3130708303fc55088e5dd582
Georgiy <valyaev2002@bk.ru>
2021-09-30 15:53:47 +0300

    Create second Android app

---
+++ Sec_app_layouts/app/src/main/java/com/example/sec_app_layouts/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.sec_app_layouts;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.myscreen);
+    }
+}
